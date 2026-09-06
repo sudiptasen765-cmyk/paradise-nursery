114e8cc2e4bd70183685d23834ad3284da40838a
sudiptasen765-cmyk <sudiptasen765@gmail.com>
2026-09-06 17:56:05 +0530

    Add AboutUs component with company information

---
+++ AboutUs.jsx
@@ -0,0 +1,24 @@
+function AboutUs() {
+  return (
+    <div className="about-us">
+      <h1>About Paradise Nursery</h1>
+
+      <p>
+        Paradise Nursery is a houseplant company dedicated to bringing
+        nature and greenery into homes and offices.
+      </p>
+
+      <p>
+        We offer a wide variety of beautiful, healthy, and easy-to-care-for
+        indoor and outdoor plants for plant lovers of all experience levels.
+      </p>
+
+      <p>
+        Our goal is to make it easy for everyone to create a greener,
+        healthier, and more beautiful living space.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
