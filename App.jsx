bfe84aecd27798dbfe07a7055d2a3ab310ff34f1
sudiptasen765-cmyk <sudiptasen765@gmail.com>
2026-09-06 17:58:21 +0530

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="landing-page">
+      <div className="landing-content">
+        <h1>Paradise Nursery</h1>
+        <p>Bring nature into your home with our beautiful houseplants.</p>
+        <button className="get-started">Get Started</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
